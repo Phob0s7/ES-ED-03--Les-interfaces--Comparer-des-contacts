b17821f5ca6920584a48fbe75038134bbba6a785
Sylvain <Villsyl@divtec.ch>
2019-05-08 19:21:06 +0200

    Bugs : - Trie impossible sur la date de naissance

---
+++ ES-ED-03-Comparer des contacts/Contact.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES_ED_03_Comparer_des_contacts
+{
+	public class Contact : IComparable<Contact>
+	{
+		public string Nom { get; set; }
+		public string Prénom { get; set; }
+		public string Email { get; set; }
+
+		private DateTime dateNaissance;
+
+		/// <summary>
+		/// Construit un contact.
+		/// </summary>
+		/// <param name="nom">Nom du contact.</param>
+		/// <param name="prénom">Prénom du contact.</param>
+		/// <param name="jourNaissance">Jour de naissance du contact.</param>
+		/// <param name="moisNaissance">Mois de naissance du contact.</param>
+		/// <param name="annéeNaissance">Année de naissance du contact.</param>
+		/// <param name="email">Email du contact.</param>
+
+		public Contact(string nom,
+					   string prénom,
+					   int jourNaissance,
+					   int moisNaissance,
+					   int annéeNaissance,
+					   string email = "")
+		{
+			Nom = nom;
+			Prénom = prénom;
+			dateNaissance = new DateTime(annéeNaissance,
+										 moisNaissance,
+										 jourNaissance);
+			Email = email;
+		}
+
+		/// <summary>
+		/// Décrit ce contact.
+		/// </summary>
+		/// <returns>une chaîne de caractères contenant une description de ce contact.</returns>
+		public override string ToString()
+		{
+			return string.Format("{0} {1}, né le {2}, {3}", Nom, Prénom, dateNaissance.ToShortDateString(), Email);
+		}
+
+		public int CompareTo(Contact other)
+		{
+			if (Nom == other.Nom || Prénom == other.Prénom)
+			{
+				Prénom.CompareTo(other.Prénom);
+				return dateNaissance.CompareTo(other.dateNaissance);
+			}
+
+			return Nom.CompareTo(other.Nom);
+		}
+	}
+}
+
--- ES-ED-03-Comparer des contacts/Program.cs
+++ ES-ED-03-Comparer des contacts/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace ES_ED_03_Comparer_des_contacts
 {
-	class Program
+	class Program 
 	{
+		/// <summary>
+		/// Point d'entrée du programme.
+		/// </summary>
 		static void Main(string[] args)
 		{
+			List<Contact> liste = new List<Contact>()
+			{
+				new Contact("Vaillant", "Michel", 25, 3, 1940, "michel@vaillant.fr"),
+				new Contact("Brisfer", "Benoît", 7, 12, 2010, "bbris1133@caramail.fr"),
+				new Contact("Caillau", "Khéna", 7, 6, 2000, "khena_c@hotmail.com"),
+				new Contact("Champignac", "Pacôme Hégésippe Adélard Ladislas",28, 6, 1930, ""),
+				new Contact("Caillau", "Georges", 17, 6, 1970, "g.caillau@gmail.com"),
+				new Contact("Vaillant", "Jean-Pierre", 15, 9, 1938,"jpierre@vaillant.fr"),
+				new Contact("Vaillant", "Michel", 28, 11, 1968,"michel_junior@vaillant.fr")
+			};
+
+			Console.WriteLine("Liste non-triée :");
+			DisplayList(liste);
+
+			liste.Sort();
+
+			Console.WriteLine("Liste triée :");
+			DisplayList(liste);
+
+		}
+
+		/// <summary>
+		/// Affiche la liste de contacts donnée.
+		/// </summary>
+		/// <param name="liste">Liste des contacts à afficher.</param>
+		public static void DisplayList(IList<Contact> liste)
+		{
+			Console.WriteLine(string.Join("\n", liste));
+			Console.WriteLine();
 		}
 	}
 }
